b3688382b6005663cb1fdfd40e5ede7d1d9356a2
Ferdi Durgun <65498175+ferdidrgn@users.noreply.github.com>
2024-11-25 02:06:12 +0300

    Update 28.02.2020.sql

--- 28.02.2020.sql
+++ 28.02.2020.sql
@@ -1,27 +1,27 @@
-/* Basit Örnekler vardý ve yoktum. Like komutu, sýralama falan */
+/* Basit Ã–rnekler Like komutu, sÄ±ralama falan yetiÅŸemedim */
 
 select SUBSTRING(SalesOrderNumber,3,4) from Sales.SalesOrderHeader Where SUBSTRING(SalesOrderNumber,3,4)= 4520
 select * from Sales.SalesOrderHeader Where SUBSTRING(SalesOrderNumber,3,4)= 4520
-select SUBSTRING(SalesOrderNumber,4,4) from Sales.SalesOrderHeader /* Soldan 4 falan (çözemedim...)*/
+select SUBSTRING(SalesOrderNumber,4,4) from Sales.SalesOrderHeader /* Soldan 4 falan (Ã§Ã¶zemedim...)*/
 select SalesOrderNumber , RIGHT(SalesOrderNumber,4) from Sales.SalesOrderHeader
 select SalesOrderNumber , LEFT(SalesOrderNumber,4) from Sales.SalesOrderHeader
-select LOWER(PurchaseOrderNumber) from Sales.SalesOrderHeader /*Harfleri küçük þekilde yazdýrma*/
-select distinct LOWER(PurchaseOrderNumber) from Sales.SalesOrderHeader Where PurchaseOrderNumber like LOWER('po%') /*Harfleri küçük þekilde yazdýrma ve baþý po ile baþlayan*/
-select distinct LOWER(PurchaseOrderNumber) from Sales.SalesOrderHeader Where PurchaseOrderNumber like LOWER('%po%')/*Harfleri küçük þekilde yazdýrma ve baþý po ile geçen*/
-select distinct upper(PurchaseOrderNumber) from Sales.SalesOrderHeader Where PurchaseOrderNumber like LOWER('%po%') /*Harfleri büyüttü*/
+select LOWER(PurchaseOrderNumber) from Sales.SalesOrderHeader /*Harfleri kÃ¼Ã§Ã¼k ÅŸekilde yazdÄ±rma*/
+select distinct LOWER(PurchaseOrderNumber) from Sales.SalesOrderHeader Where PurchaseOrderNumber like LOWER('po%') /*Harfleri kÃ¼Ã§Ã¼k ÅŸekilde yazdÄ±rma ve baÅŸÄ± po ile baÅŸlayan*/
+select distinct LOWER(PurchaseOrderNumber) from Sales.SalesOrderHeader Where PurchaseOrderNumber like LOWER('%po%')/*Harfleri kÃ¼Ã§Ã¼k ÅŸekilde yazdÄ±rma ve baÅŸÄ± po ile geÃ§en*/
+select distinct upper(PurchaseOrderNumber) from Sales.SalesOrderHeader Where PurchaseOrderNumber like LOWER('%po%') /*Harfleri bÃ¼yÃ¼ttÃ¼*/
 select distinct upper(PurchaseOrderNumber) from Sales.SalesOrderHeader Where PurchaseOrderNumber like LOWER('%po%') order by UPPER(PurchaseOrderNumber) desc
 select distinct LEN(OrderDate) from Sales.SalesOrderHeader /*Karakter Sayma*/
-select distinct LEN(ltrim(OrderDate)) from Sales.SalesOrderHeader /*Karakter Sayma ve boþluklarý kaldýrma (ama date de boþluk yokmuþ!)*/
-select  CONCAT(OrderDate, ' * ', DueDate) from Sales.SalesOrderHeader -- Birleþtirme, bir sütunda yan yana yazdýrma
-select REVERSE(SalesOrderNumber) from Sales.SalesOrderHeader --Tersten yazdýrma 
-select ABS(SubTotal) from Sales.SalesOrderHeader --Mutlak Deðer
+select distinct LEN(ltrim(OrderDate)) from Sales.SalesOrderHeader /*Karakter Sayma ve boÅŸluklarÄ± kaldÄ±rma (ama date de boÅŸluk yokmuÅŸ!)*/
+select  CONCAT(OrderDate, ' * ', DueDate) from Sales.SalesOrderHeader -- BirleÅŸtirme, bir sÃ¼tunda yan yana yazdÄ±rma
+select REVERSE(SalesOrderNumber) from Sales.SalesOrderHeader --Tersten yazdÄ±rma 
+select ABS(SubTotal) from Sales.SalesOrderHeader --Mutlak DeÃ°er
 select UPPER(LEFT(PurchaseOrderNumber,1))+ LOWER(substring(PurchaseOrderNumber,2,LEN(PurchaseOrderNumber))) from Sales.SalesOrderHeader --????
 select ROUND(TotalDue,5) from Sales.SalesOrderHeader --???
 select SUM(TotalDue) from Sales.SalesOrderHeader
 select Avg(TotalDue) from Sales.SalesOrderHeader -- Ortalama
-select MAX(TotalDue) from Sales.SalesOrderHeader --Maximum Deðeri
-select min(TotalDue) from Sales.SalesOrderHeader --Minimum Deðeri
-select COUNT (*) from Sales.SalesOrderHeader     -- Sütun ve satýrlarý (tümünü) saydý
+select MAX(TotalDue) from Sales.SalesOrderHeader --Maximum DeÃ°eri
+select min(TotalDue) from Sales.SalesOrderHeader --Minimum DeÃ°eri
+select COUNT (*) from Sales.SalesOrderHeader     -- SÃ¼tun ve satÄ±rlarÄ± (tÃ¼mÃ¼nÃ¼) saydÄ±
 
 
 
